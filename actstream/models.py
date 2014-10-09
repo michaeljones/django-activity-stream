6ad644067c43d88fc11062d899848d491234fcc2
Michael Jones <m.pricejones@gmail.com>
2014-10-09 10:37:05 +0100

    Add 'site' foreign key relations to models
    We want to be able to associate actions and 'following' with specific
    sites rather than having them global.
    
    This is a breaking change as it requires use of the site system. We
    might be able to able null's in this field in order to make the support
    optional. I'm not sure if that is for the best.
    
    The django comments framework simply requires 'sites' and so does
    'django-allauth' so there is some precedent for requiring it even for
    people who have single sites.

--- actstream/models.py
+++ actstream/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext as _
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.timesince import timesince as djtimesince
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.sites.models import Site
 
 
 try:
@@ -32,6 +33,8 @@ class Follow(models.Model):
     actor_only = models.BooleanField("Only follow actions where "
                                      "the object is the target.", default=True)
     started = models.DateTimeField(default=now)
+    site = models.ForeignKey(Site)
+
     objects = FollowManager()
 
     class Meta:
@@ -93,6 +96,8 @@ class Action(models.Model):
     timestamp = models.DateTimeField(default=now)
 
     public = models.BooleanField(default=True)
+
+    site = models.ForeignKey(Site)
 
     objects = actstream_settings.get_action_manager()
 
